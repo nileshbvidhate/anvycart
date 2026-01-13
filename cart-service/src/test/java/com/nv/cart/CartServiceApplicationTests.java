9916d4105759b6fef943f5492f0f2f5998f50e7e
nileshbvidhate <nileshbvidhate@gmail.com>
2026-01-13 17:01:20 +0530

    add cart service skeleton

---
+++ cart-service/src/test/java/com/nv/cart/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nv.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
